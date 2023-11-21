a5defb8a0bd4829e1187704d9d7fab120cb05965
王文斌 <wwb123123@outlook.com>
2023-11-22 01:24:50 +0800

    更新控制台模板项目

---
+++ templates/ZStack.Framework.Templates.ConsoleApp/src/GlobalUsings.cs
@@ -0,0 +1,7 @@
+﻿global using Microsoft.Extensions.DependencyInjection;
+global using Serilog;
+global using ZStack.Core;
+global using ZStack.Core.Attributes;
+global using ZStack.Core.Utils;
+global using ZStack.Extensions;
+global using ZStack.Extensions.Models;
