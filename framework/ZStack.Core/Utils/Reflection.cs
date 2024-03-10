6b72f766eec0e19c6e7d751c32093277695ed21e
王文斌 <wwb123123@outlook.com>
2024-03-10 21:49:28 +0800

    feat: 增加静态工具方法-扫描添加程序集

--- framework/ZStack.Core/Utils/Reflection.cs
+++ framework/ZStack.Core/Utils/Reflection.cs
@@ -28,10 +28,23 @@ public static class Reflection
     /// 加载 ZStack 程序集
     /// </summary>
     public static void LoadZStackAssemblies()
+        => LoadAssemblies(["ZStack.*.dll"]);
+
+    /// <summary>
+    /// 加载程序集
+    /// </summary>
+    /// <param name="includePatterns"></param>
+    /// <param name="excludePatterns"></param>
+    /// <param name="directory"></param>
+    public static void LoadAssemblies(string[] includePatterns, string[]? excludePatterns = null, string? directory = null)
     {
-        var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        if (includePatterns.Length == 0)
+            return;
+        var baseDir = directory ?? AppDomain.CurrentDomain.BaseDirectory;
         var mather = new Matcher(StringComparison.OrdinalIgnoreCase);
-        mather.AddIncludePatterns(["ZStack.*.dll"]);
+        mather.AddIncludePatterns(includePatterns);
+        if (excludePatterns != null && excludePatterns.Length > 0)
+            mather.AddExcludePatterns(excludePatterns);
         var result = mather.Execute(new DirectoryInfoWrapper(new DirectoryInfo(baseDir)));
         var files = result.Files.Select(x => x.Path).ToList();
         var assemblies = GetAssemblies();
