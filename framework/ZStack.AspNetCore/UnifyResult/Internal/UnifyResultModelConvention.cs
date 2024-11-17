a8f2f563214ac67315d77376112f7babc9054935
王文斌 <wwb123123@outlook.com>
2024-11-17 19:14:22 +0800

    fix: 未引入命名空间修复

--- framework/ZStack.AspNetCore/UnifyResult/Internal/UnifyResultModelConvention.cs
+++ framework/ZStack.AspNetCore/UnifyResult/Internal/UnifyResultModelConvention.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using System.Reflection;
 
 namespace ZStack.AspNetCore.UnifyResult;
 
