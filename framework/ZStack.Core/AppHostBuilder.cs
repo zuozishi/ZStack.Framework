3757fe431adf3fce31a813b2d2c841246e3697a6
王文斌 <wwb123123@outlook.com>
2025-11-07 21:57:51 +0800

    feat: 优化AppHostBuilder

--- framework/ZStack.Core/AppHostBuilder.cs
+++ framework/ZStack.Core/AppHostBuilder.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileSystemGlobbing;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Serilog;
@@ -19,16 +18,24 @@ public static class AppHostBuilder
     /// <param name="serilogConfiguration"></param>
     /// <param name="consoleLoggerTemplate"></param>
     /// <returns></returns>
-    public static HostApplicationBuilder CreateHostBuilder(string[]? args = null, Action<LoggerConfiguration>? serilogConfiguration = null, string? consoleLoggerTemplate = "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}][{ShortSourceContext}] {Message:lj}{NewLine}{Exception}")
+    public static HostApplicationBuilder CreateHostBuilder(
+        string[]? args = null,
+        Action<IConfigurationManager, IHostEnvironment>? configuration = null,
+        Action<LoggerConfiguration, IHostEnvironment>? serilogConfiguration = null,
+        string? consoleLoggerTemplate = "[{Timestamp:yyyy-MM-dd HH:mm:ss} {Level:u3}][{ShortSourceContext}] {Message:lj}{NewLine}{Exception}")
     {
         var builder = Host.CreateApplicationBuilder(args);
-        ConfigureConfiguration(builder.Environment, builder.Configuration);
+
+        if (configuration != null)
+            configuration(builder.Configuration, builder.Environment);
+        else
+            DefaultConfigureConfiguration(builder.Configuration, builder.Environment);
 
         Log.Logger = SerilogLogger.CreateConfigurationLogger(builder.Configuration, configure =>
         {
             if (!string.IsNullOrEmpty(consoleLoggerTemplate))
                 configure.WriteTo.Console(outputTemplate: consoleLoggerTemplate);
-            serilogConfiguration?.Invoke(configure);
+            serilogConfiguration?.Invoke(configure, builder.Environment);
         });
         builder.Logging.ClearProviders();
         builder.Logging.AddSerilog(Log.Logger);
@@ -39,42 +46,15 @@ public static class AppHostBuilder
     }
 
     /// <summary>
-    /// 配置选项
+    /// 默认配置选项
     /// </summary>
     /// <param name="environment"></param>
     /// <param name="configuration"></param>
-    internal static void ConfigureConfiguration(IHostEnvironment environment, IConfigurationManager configuration)
+    internal static void DefaultConfigureConfiguration(IConfigurationManager configuration, IHostEnvironment environment)
     {
-        var env = environment.EnvironmentName;
         var configurationDirectory = Path.Combine(AppDomain.CurrentDomain.BaseDirectory,
             configuration["ConfigurationDirectory"] ?? "Configuration");
-        var files = new Matcher()
-            .AddInclude("*.json")
-            .AddInclude("*.ini")
-            .AddInclude("*.yml")
-            .AddInclude("*.yaml")
-            .AddExclude("*.schema.json")
-            .GetResultsInFullPath(configurationDirectory);
-        foreach (var file in files)
-        {
-            var fileName = Path.GetFileName(file);
-            var pt = fileName.Split('.');
-            if (pt.Length > 2 && !env.Equals(pt[^2], StringComparison.OrdinalIgnoreCase))
-                continue;
-            if (fileName.EndsWith(".json", StringComparison.OrdinalIgnoreCase))
-            {
-                configuration.AddJsonFile(file, optional: true, reloadOnChange: true);
-            }
-            else if (fileName.EndsWith(".ini", StringComparison.OrdinalIgnoreCase))
-            {
-                configuration.AddIniFile(file, optional: true, reloadOnChange: true);
-            }
-            else if (fileName.EndsWith(".yml", StringComparison.OrdinalIgnoreCase) ||
-                fileName.EndsWith(".yaml", StringComparison.OrdinalIgnoreCase))
-            {
-                configuration.AddYamlFile(file, optional: true, reloadOnChange: true);
-            }
-        }
+        configuration.AddDirectory(configurationDirectory, environment);
         configuration.AddEnvironmentVariables();
         configuration.AddCommandLine(Environment.GetCommandLineArgs());
     }
