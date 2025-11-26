e74580dcd7173bfa884ac221dc83a43824a552cd
王文斌 <wwb123123@outlook.com>
2025-11-26 22:36:51 +0800

    更新依赖版本并新增 TempFile 类
    更新多个项目文件中的依赖包版本：
    - 将 `Directory.Build.props` 中的版本号从 `10.0.3` 升级到 `10.0.4`。
    - 升级 `ZStack.AspNetCore.OpenTelemetry.csproj` 中的 OpenTelemetry 相关依赖到 `1.14.0`。
    - 升级 `ZStack.AspNetCore.SqlSugar.csproj`、`ZStack.SqlSugar.csproj` 和 `WebApplication.csproj` 中的 `SqlSugarCoreNoDrive` 到 `5.1.4.210`。
    - 升级 `ZStack.AspNetCore.csproj` 中的 `Scalar.AspNetCore` 到 `2.11.0`。
    
    新增 `TempFile` 类，用于创建和管理临时文件，支持通过字符串、字符串数组或字节数组初始化文件内容，并在释放时自动删除文件。

---
+++ framework/ZStack.Core/IO/TempFile.cs
@@ -0,0 +1,37 @@
+﻿namespace ZStack.Core.IO;
+
+public class TempFile : IDisposable
+{
+    public string FilePath { get; }
+
+    public TempFile()
+    {
+        FilePath = Path.GetTempFileName();
+    }
+
+    public TempFile(string content)
+    {
+        FilePath = Path.GetTempFileName();
+        File.WriteAllText(FilePath, content);
+    }
+
+
+    public TempFile(string[] content)
+    {
+        FilePath = Path.GetTempFileName();
+        File.WriteAllLines(FilePath, content);
+    }
+
+    public TempFile(byte[] content)
+    {
+        FilePath = Path.GetTempFileName();
+        File.WriteAllBytesAsync(FilePath, content);
+    }
+
+    public void Dispose()
+    {
+        if (File.Exists(FilePath))
+            File.Delete(FilePath);
+        GC.SuppressFinalize(this);
+    }
+}
